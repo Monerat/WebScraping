8c726a659b5a620d76a659cf7d2b9a57d336ecfc
Monerat <gmonerat@gmail.com>
2021-02-18 19:25:17 -0300

    Adicionado acoes de cada setor, atualizado o FuncAcao2

--- WebScraping/SiteStatusInvest/FuncAcao2.py
+++ WebScraping/SiteStatusInvest/FuncAcao2.py
@@ -1,7 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
+from FormulasValuation import GrahamRev
 import re
-
+import pandas as pd
+import numpy as np
 
 def farejadorStrong(Soup,Codigo):
     if(Soup.find(string=Codigo).find_next("strong").contents[0] == None):
@@ -24,6 +26,39 @@ def WebScrappingAcao2(Codigo):
         Data.close()
 
     return Indicadores
+
+def dfAcoes(Nometxt):
+    AllStocks=[]
+    with open(Nometxt,"r") as Data:
+        Linhas = Data.read().split("\n")
+        for n in Linhas:
+            AllStocks.append(WebScrappingAcao2(n))
+        Data.close()
+
+    df = pd.DataFrame(AllStocks)
+    df.columns=["Cod Acao","Valor atual","D.Y","P/L","PEG Ratio","P/VP","EV/EBITDA","EV/EBIT","P/EBITDA",
+    "P/EBIT","VPA","P/Ativo","LPA","P/SR","P/Cap. Giro","P/Ativo Circ. Liq.","Dív. líquida/PL",
+    "Dív. líquida/EBITDA","Dív. líquida/EBIT","PL/Ativos","Passivo/Ativos","Liq. corrente","M. Bruta",
+    "M. EBITDA","M. EBITDA","M. Líquida","ROE","ROA","ROIC","Giro ativos","CAGR Receitas 5 anos",
+    "CAGR Lucros 5 anos","Patrimônio líquido","Ativos","Ativo circulante","Dívida bruta","Disponibilidade",
+    "Dívida líquida","Valor de mercado","Valor de firma","Nº total de papeis"]
+
+    valuation=[]
+    Selic = 2
+
+    for linha in range(0, len(df.index), 4):
+        
+        valuation.append(GrahamRev(df.iloc[linha,12],df.iloc[linha,31],Selic))
+        valuation.append(GrahamRev(df.iloc[linha+1,12],df.iloc[linha+1,31],Selic))
+        valuation.append(GrahamRev(df.iloc[linha+2,12],df.iloc[linha+2,31],Selic))
+        valuation.append(GrahamRev(df.iloc[linha+3,12],df.iloc[linha+3,31],Selic))
+
+    df['Valuation Graham'] = valuation
+    df['Valuation Graham'] = df['Valuation Graham'].astype(str).str.replace('.',',',regex=True)
+    
+    return df
+
+
 '''
 acao =[]
 acao.append(WebScrappingAcao2("ITSA4"))
--- WebScraping/SiteStatusInvest/Main.py
+++ WebScraping/SiteStatusInvest/Main.py
@@ -1,39 +1,18 @@
-from FuncAcao import WebScrappingAcao
 from FuncAcao2 import WebScrappingAcao2
+from FuncAcao2 import dfAcoes
 from FuncBDR import WebScrappingBDR
 from FuncFII import WebScrappingFII
-from FormulasValuation import GrahamRev
 import pandas as pd
 import numpy as np
 
-AllStocks=[]
-with open("CodAcoes.txt","r") as Data:
+
+
+with open("CodAcoesGeral.txt","r") as Data:
     Linhas = Data.read().split("\n")
     for n in Linhas:
-        AllStocks.append(WebScrappingAcao2(n))
+        df = dfAcoes(n)
+        df.to_csv(r"C:\Users\gmone\Desktop\EstudoAcoes\BancodeDados\BancoDadosAcoes"+n+r".csv",index=False)
     Data.close()
-
-df = pd.DataFrame(AllStocks)
-df.columns=["Cod Acao","Valor atual","D.Y","P/L","PEG Ratio","P/VP","EV/EBITDA","EV/EBIT","P/EBITDA",
-"P/EBIT","VPA","P/Ativo","LPA","P/SR","P/Cap. Giro","P/Ativo Circ. Liq.","Dív. líquida/PL",
-"Dív. líquida/EBITDA","Dív. líquida/EBIT","PL/Ativos","Passivo/Ativos","Liq. corrente","M. Bruta",
-"M. EBITDA","M. EBITDA","M. Líquida","ROE","ROA","ROIC","Giro ativos","CAGR Receitas 5 anos",
-"CAGR Lucros 5 anos","Patrimônio líquido","Ativos","Ativo circulante","Dívida bruta","Disponibilidade",
-"Dívida líquida","Valor de mercado","Valor de firma","Nº total de papeis"]
-
-valuation=[]
-Selic = 2
-
-for linha in range(0, len(df.index), 4):
-    valuation.append(GrahamRev(df.iloc[linha,12],df.iloc[linha,31],Selic))
-    valuation.append(GrahamRev(df.iloc[linha+1,12],df.iloc[linha+1,31],Selic))
-    valuation.append(GrahamRev(df.iloc[linha+2,12],df.iloc[linha+2,31],Selic))
-    valuation.append(GrahamRev(df.iloc[linha+3,12],df.iloc[linha+3,31],Selic))
-
-df['Valuation Graham'] = valuation
-df['Valuation Graham'] = df['Valuation Graham'].astype(str).str.replace('.',',')
-print(df)
-df.to_csv(r"C:\Users\gmone\Desktop\EstudoAcoes\BancodeDados\BancoDadosAcoes.csv",index=False)
 
 AllBDRs=[]
 with open("CodBDRs.txt","r") as Data:
